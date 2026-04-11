d44453594f4366f3497dbd42bf3a8d9c4a1eb69d
Victor Deleeck <victor@deleeck.be>
2026-04-11 22:27:41 +0200

    Initial Commit

---
+++ pushup/pushupApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushupApp.swift
+//  pushup
+//
+//  Created by Victor Deleeck on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct pushupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
